7d8a620cbf0630f9896f5764f765b48bb9a64b59
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-03-15 17:36:31 -0300

    feat: creates migration for table 'users'

---
+++ src/migrations/20230315201940-create-users.js
@@ -0,0 +1,40 @@
+/**
+* @param {import('sequelize').Sequelize} sequelize
+* @param {import('sequelize').DataTypes} sequelize.DataTypes
+*/
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'display_name',
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+  return queryInterface.dropTable('users');
+  }
+};
