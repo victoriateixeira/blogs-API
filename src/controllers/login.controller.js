0cc64e1450355fff23ba15402066a38979df2121
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-03-15 18:39:22 -0300

    feat: crates userService and login controller

---
+++ src/controllers/login.controller.js
@@ -0,0 +1,28 @@
+const { userService } = require('../services');
+require('dotenv/config');
+
+const isBodyValid = (email, password) => email && password;
+
+module.exports = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!isBodyValid(email, password)) {
+      return res.status(401).json({ message: 'Some required fields are missing' });
+    }
+
+    const user = await userService.getByEmail(email);
+
+    if (!user || password !== user.password) {
+      return res.status(400).json({ message: 'Invalid fields' });
+    }
+
+    const { password: _, ...userWithoutPassword } = user.dataValues;
+
+    const token = createToken(userWithoutPassword);
+    res.status(200).json({ token });
+  } catch (err) {
+     return res
+      .status(500)
+      .json({ message: 'Erro interno', error: err.message });
+  }
+};
