7e18885bd76de59944e689aa84f29adf95aba50e
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-03-15 17:45:04 -0300

    feat: creates migration for table 'categories'

---
+++ src/migrations/20230315203741-create-categories.js
@@ -0,0 +1,26 @@
+/**
+* @param {import('sequelize').Sequelize} sequelize
+* @param {import('sequelize').DataTypes} sequelize.DataTypes
+*/
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('categories', {
+id: {
+  allowNull: false,
+  autoIncrement: true,
+  primaryKey: true,
+  type: Sequelize.INTEGER,
+},
+name: {
+  allowNull: false,
+  type: Sequelize.STRING,
+},
+    });
+  
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('categories');
+  }
+};
