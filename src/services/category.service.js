5e418f04d62fcbfe2d669d5ab73bd6bdbefa12c9
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-03-17 11:17:18 -0300

    feat: adding getAllCategories function to categoryService

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -5,6 +5,12 @@ const newCategory = await Category.create({ name });
 return { type: null, message: newCategory };
 };
 
+const getAllCategories = async () => {
+  const allCategories = await Category.findAll();
+  return { type: null, message: allCategories };
+};
+
 module.exports = {
   createCategory,
+  getAllCategories, 
 };
