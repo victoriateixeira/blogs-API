0f2a28774a0c1135c9ea13189cfc7b12ebee3e39
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-03-17 06:53:22 -0300

    feat: adds getById function to userController

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -20,7 +20,19 @@ const allUsers = await userService.getAllUsers();
 return res.status(200).json(allUsers);
 };
 
+const getById = async (req, res) => {
+const { id } = req.params;
+const user = await userService.getById(id);
+if (!user) {
+ return res.status(404).json({
+  message: 'User does not exist',
+}); 
+}
+return res.status(200).json(user);
+};
+
 module.exports = {
   createUser,
   getAllUsers,
+  getById,
 };
