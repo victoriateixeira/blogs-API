79ab1f99aef9792b5ca726868b08abfb147b45f4
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-03-17 11:21:15 -0300

    feat: adds getAllCategories to categoryRouter

--- src/routers/categoryRouter.js
+++ src/routers/categoryRouter.js
@@ -5,4 +5,5 @@ const validateToken = require('../middlewares/validateToken');
 
 const router = express.Router();
 router.post('/', validateToken, categoryController.createCategory);
+router.get('/', validateToken, categoryController.getAllCategories);
 module.exports = router;
