4be5df19cdcb1ab875241b8b3187b93d76bc263a
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-03-20 13:14:28 -0300

    feat: reviews blogPost.controller

--- src/controllers/blogPost.controller.js
+++ src/controllers/blogPost.controller.js
@@ -1,10 +1,13 @@
 const { blogPostService } = require('../services');
 
 const createBlogPost = async (req, res) => {
-  const { title, content, categoryIds, userId } = req.body;
-  const newBlogPost = await blogPostService.createBlogPost({ title, content, categoryIds, userId });
+  const { title, content, categoryIds } = req.body;
+  const { userId } = req.userId;
+  const newBlogPost = await blogPostService.createBlogPost({ title, content, userId });
+  await Promise.all(categoryIds
+    .map((category) => blogPostService.createPostCategory({ postId: newBlogPost.id, category })));
   if (newBlogPost.type) { return res.status(newBlogPost.type).json(newBlogPost.message); }
-  return res.status(201).json(newBlogPost.message);
+  return res.status(201).json(newBlogPost);
 };
 
 module.exports = {
