2fb173d0c5f462c4b6c25513eb2e06522110598b
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-03-20 18:56:25 -0300

    feat: delete user func complete

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,4 +7,5 @@ const router = express.Router();
 router.post('/', userController.createUser);
 router.get('/', validateToken, userController.getAllUsers);
 router.get('/:id', validateToken, userController.getById);
+router.delete('/me', validateToken, userController.deleteUserMe);
 module.exports = router;
